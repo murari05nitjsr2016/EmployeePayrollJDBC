f4199e8f0f32d7c9df43e32382581b86af7e8e15
murari05nitjsr2016 <murari05nitjsr2016@gmail.com>
2021-05-02 12:16:51 +0530

    added:welcome msg

---
+++ main/java/com/jdbc/emppayrollservice/EmployeePayRollService.java
@@ -0,0 +1,7 @@
+package com.jdbc.emppayrollservice;
+
+public class EmployeePayRollService {
+    public static void main(String[] args) {
+        System.out.println("welcome in employee payroll system");
+    }
+}
