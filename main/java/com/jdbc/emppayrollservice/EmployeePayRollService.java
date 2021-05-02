c6537d4bef4df13003919f389cec94f836e649b8
murari05nitjsr2016 <murari05nitjsr2016@gmail.com>
2021-05-03 00:52:59 +0530

    added:insert row into table

--- main/java/com/jdbc/emppayrollservice/EmployeePayRollService.java
+++ main/java/com/jdbc/emppayrollservice/EmployeePayRollService.java
@@ -1,8 +1,5 @@
 package com.jdbc.emppayrollservice;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class EmployeePayRollService {
     static final String userName = "root";
@@ -37,9 +34,21 @@ public class EmployeePayRollService {
                 " PRIMARY KEY ( id ))";
         statement.executeUpdate(sql);
     }
+
+    public static void insertRow() throws SQLException {
+        statement = connection.createStatement();
+        String insertQuery = "INSERT INTO employee_payroll (name, salary, start) VALUES (?, ?, ?)";
+        PreparedStatement pstmt = connection.prepareStatement(insertQuery);
+        pstmt.setString(1,"Bill");
+        pstmt.setDouble(2,1000000.0);
+        pstmt.setDate(3, Date.valueOf("2018-01-03"));
+        pstmt.execute();
+
+    }
     public static void main(String[] args) throws SQLException {
         System.out.println("welcome in employee payroll system");
         loadAndEstablishConnectionWithDB();
-        createTableWithAttributes();
+        insertRow();
+
     }
 }
