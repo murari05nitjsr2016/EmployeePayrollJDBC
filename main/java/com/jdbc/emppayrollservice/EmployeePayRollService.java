1bc1f6c25fd40dce11059c76afbf8a645a647252
murari05nitjsr2016 <murari05nitjsr2016@gmail.com>
2021-05-02 21:16:45 +0530

    added:create table with attributes

--- main/java/com/jdbc/emppayrollservice/EmployeePayRollService.java
+++ main/java/com/jdbc/emppayrollservice/EmployeePayRollService.java
@@ -1,17 +1,19 @@
 package com.jdbc.emppayrollservice;
-
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class EmployeePayRollService {
+    static final String userName = "root";
+    static final String passWord = "root";
+    static final String jdbcURL = "jdbc:mysql://localhost:3306/payroll_service";
+    public  static Connection connection;
+    public static Statement statement;
+
     public static  void loadAndEstablishConnectionWithDB()
     {
-        String jdbcURL = "jdbc:mysql://localhost:3306/payroll_service";
-        String userName = "root";
-        String passWord = "root";
-        Connection connection;
+
         try {
             Class.forName("com.mysql.jdbc.Driver");
             System.out.println("Driver sucessfully Loaded");
@@ -24,8 +26,20 @@ public class EmployeePayRollService {
             System.out.println("connection failed");
         }
     }
-    public static void main(String[] args) {
+
+    public static void createTableWithAttributes() throws SQLException {
+        statement = connection.createStatement();
+        String sql = "CREATE TABLE employee_payroll " +
+                "(id INTEGER not NULL auto_increment, " +
+                " name VARCHAR(150), " +
+                " salary double not null, " +
+                " start date not null, " +
+                " PRIMARY KEY ( id ))";
+        statement.executeUpdate(sql);
+    }
+    public static void main(String[] args) throws SQLException {
         System.out.println("welcome in employee payroll system");
         loadAndEstablishConnectionWithDB();
+        createTableWithAttributes();
     }
 }
